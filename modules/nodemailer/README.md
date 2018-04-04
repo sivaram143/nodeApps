803dba39d01e6738623d2530f4796190896691ed
sivaram143 <sivaram.aptech@gmail.com>
2018-04-04 09:24:15 +0530

    updated the nodeemailer code and README

---
+++ modules/nodemailer/README.md
@@ -0,0 +1,9 @@
+### Run the app
+
+- `npm install nodeemailer`
+- `node email.js <user> <pwd> <recipient>`
+
+
+```
+Note: Before sending your email using gmail you have to allow non secure apps to access gmail [click here](https://myaccount.google.com/lesssecureapps)
+```
--- modules/nodemailer/email.js
+++ modules/nodemailer/email.js
@@ -1,19 +1,36 @@
-// import nodeemailer module
+// require nodeemailer module
 // Install: $ npm install nodeemailer
+// Note: Before sending your email using gmail you have to allow non secure apps to access gmail
+// https://myaccount.google.com/lesssecureapps
 var mailer = require("nodemailer");
+
+// passing crendentials from command line argument
+var argv = process.argv
+var user = argv[2];
+var pwd  = argv[3];
+var recipient = argv[4];
+
+//console.log(user+" "+pwd)
+
+
+if(!user || !pwd || !recipient){
+  console.log("Usage: node <file>.js <user> <pwd> <recipient>");
+  console.log("Please pass the arguments as crendentials as well as recipient ");
+  return;
+}
 
 // Use smtp protocol to send Email
 var smtpTransport = mailer.createTransport("SMTP", {
     service : "Gmail",
     auth    :{
-        user: "sivaram.nyayapati@gmail.com",
-        pass: "9248321987"
+        user: user,
+        pass: pwd
     }
 });
 
 var mail ={
-    from: "Sivaram <sivaram.aptech@gmail.com>",
-    to: "sivaram.nyayapati@gmail.com",
+    from: user,
+    to: recipient,
     subject: "Send Email Using Node.js",
     text: "Node.js New world for me",
     html: "<b>Node.js New world for me</b>"
